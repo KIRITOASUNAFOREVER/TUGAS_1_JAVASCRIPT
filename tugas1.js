10a8e2ab1e92273d88d8a72ce177bb36319a60c5
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-03-26 13:58:44 +0700

    Tugas 1 Javascript
    Tugas ini Saya kumpulkan untuk NIOMIC

---
+++ tugas1.js
@@ -0,0 +1,7 @@
+var nama_depan = "Edward";
+let nama_akhir = "Sulistijono";
+const umur = 21;
+
+// karena umur tipe data Integer, harus ubah ke string
+//"\n" digunakan untuk NewLine
+console.log("Nama : " + nama_depan + " " + nama_akhir + "\nUmur : " + umur.toString() + " tahun");
